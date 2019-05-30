204477caf19053f2c3160d6112cba56198ae3736
guoly@ga-qu.com <12345678>
2019-05-30 16:36:39 +0800

    mqtt_ble

--- src/main/java/lib/grasp/util/AppUtil.java
+++ src/main/java/lib/grasp/util/AppUtil.java
@@ -17,6 +17,7 @@ import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
@@ -36,70 +37,6 @@ import static android.content.Context.ACTIVITY_SERVICE;
 public class AppUtil {
 
     /**
-     * 获取apk文件的ICON
-     */
-    public static Drawable getApkIcon(Context context, String apkPath) {
-        if (!FileUtil.fileExists(apkPath)) return null;
-
-        PackageManager pckManager = context.getPackageManager();
-        PackageInfo info = pckManager.getPackageArchiveInfo(apkPath, PackageManager.GET_ACTIVITIES);
-        if (info == null) return null;
-
-        ApplicationInfo appInfo = info.applicationInfo;
-        appInfo.sourceDir = apkPath;
-        appInfo.publicSourceDir = apkPath;
-        return appInfo.loadIcon(pckManager);
-    }
-
-    /**
-     * 安装APK
-     */
-    public static void installAPK(Context context, File file) {
-        if (!FileUtil.isFileExists(file)) return;
-        Uri apk = getImageContentUri(context, file);
-
-        if (Build.VERSION.SDK_INT >= 24) {
-            install(context, file);
-        } else {
-            openFile(context, file);
-        }
-
-//        else if (Build.VERSION.SDK_INT < 23) {
-//            Intent intents = new Intent();
-//            intents.setAction(Intent.ACTION_VIEW);
-////                intents.addCategory("android.intent.category.DEFAULT");
-//            intents.setDataAndType(apk, "application/vnd.android.package-archive");
-//            intents.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            context.startActivity(intents);
-//        }
-    }
-
-    /**
-     * android6.0之后的升级更新
-     */
-    public static void openFile(Context context, File file) {
-        Intent intent = new Intent();
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setAction("android.intent.action.VIEW");
-        String type = getMIMEType(file);
-        intent.setDataAndType(Uri.fromFile(file), type);
-        try {
-            context.startActivity(intent);
-        } catch (Exception var5) {
-            var5.printStackTrace();
-            Toast.makeText(context, "没有找到打开此类文件的程序", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private static String getMIMEType(File var0) {
-        String var1 = "";
-        String var2 = var0.getName();
-        String var3 = var2.substring(var2.lastIndexOf(".") + 1, var2.length()).toLowerCase();
-        var1 = MimeTypeMap.getSingleton().getMimeTypeFromExtension(var3);
-        return var1;
-    }
-
-    /**
      * 获取手机IMEI号
      * <p>
      * 需要动态权限: android.permission.READ_PHONE_STATE
@@ -109,19 +46,6 @@ public class AppUtil {
             return "";
         TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         return telephonyManager.getDeviceId();
-    }
-
-    /**
-     * android7.0之后的更新
-     * 通过隐式意图调用系统安装程序安装APK
-     */
-    public static void install(Context context, File file) {
-        Uri apkUri = FileProvider.getUriForFile(context, "com.gaqu.gaqupaopao_support.fileprovider", file);
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
-        context.startActivity(intent);
     }
 
     /**
@@ -145,6 +69,93 @@ public class AppUtil {
         // Set the component to be explicit
         explicitIntent.setComponent(component);
         return explicitIntent;
+    }
+
+    /**
+     * 获取apk文件的ICON
+     */
+    public static Drawable getApkIcon(Context context, String apkPath) {
+        if (!FileUtil.fileExists(apkPath)) return null;
+
+        PackageManager pckManager = context.getPackageManager();
+        PackageInfo info = pckManager.getPackageArchiveInfo(apkPath, PackageManager.GET_ACTIVITIES);
+        if (info == null) return null;
+
+        ApplicationInfo appInfo = info.applicationInfo;
+        appInfo.sourceDir = apkPath;
+        appInfo.publicSourceDir = apkPath;
+        return appInfo.loadIcon(pckManager);
+    }
+
+
+    /**
+     * 安装APK
+     */
+    public static void installAPK(Context context, String filePath) {
+        installAPK(context, new File(filePath));
+    }
+
+    /**
+     * 安装APK(7.0之前)
+     */
+    public static void installAPK(Context context, File file) {
+        installAPK(context, file, "");
+    }
+
+    /**
+     * 安装APK
+     */
+    public static void installAPK(Context context, String filePath, String provider) {
+        installAPK(context, new File(filePath), provider);
+    }
+
+    /**
+     * 安装APK(7.0及以后)
+     */
+    public static void installAPK(Context context, File file, String provider) {
+        if(context == null) return;
+        if (!FileUtil.isFileExists(file)) return;
+
+        if (Build.VERSION.SDK_INT >= 24) install_7(context, file, provider);
+        else install_456(context, file);
+    }
+
+    /**
+     * android6.0之后的升级更新
+     */
+    private static void install_456(Context context, File file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction("android.intent.action.VIEW");
+        String type = getMIMEType(file);
+        intent.setDataAndType(Uri.fromFile(file), type);
+        try {
+            context.startActivity(intent);
+        } catch (Exception var5) {
+            var5.printStackTrace();
+            Toast.makeText(context, "没有找到打开此类文件的程序", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private static String getMIMEType(File var0) {
+        String var1 = "";
+        String var2 = var0.getName();
+        String var3 = var2.substring(var2.lastIndexOf(".") + 1, var2.length()).toLowerCase();
+        var1 = MimeTypeMap.getSingleton().getMimeTypeFromExtension(var3);
+        return var1;
+    }
+
+    /**
+     * android7.0之后的更新
+     * 通过隐式意图调用系统安装程序安装APK
+     */
+    private static void install_7(Context context, File file, String provider) {
+        Uri apkUri = FileProvider.getUriForFile(context, provider, file);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+        context.startActivity(intent);
     }
 
     /**
@@ -375,7 +386,11 @@ public class AppUtil {
      * @param apkPath 要安装的apk文件的路径
      * @return 安装成功返回true，安装失败返回false。
      */
-    public static void installSilence(String apkPath) {
+    public static void installSilence(Context context, String apkPath) {
+        if(!isRoot()){
+            Toast.makeText(context, "当前设备暂未ROOT或者本应用未获取到设备权限", Toast.LENGTH_SHORT).show();
+            return;
+        }
         new Thread(() -> {
             boolean result = false;
             DataOutputStream dataOutputStream = null;
