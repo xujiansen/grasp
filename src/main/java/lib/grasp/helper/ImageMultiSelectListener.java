09087dbef2c5b21861a6326f23e8845ecec43513
guoly@ga-qu.com <12345678>
2019-09-24 09:53:55 +0800

    修修补补

---
+++ src/main/java/lib/grasp/helper/ImageMultiSelectListener.java
@@ -0,0 +1,10 @@
+package lib.grasp.helper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by GaQu_Dev on 2019/8/15.
+ */
+public interface ImageMultiSelectListener {
+    void onSelected(ArrayList<String> picPaths);
+}
